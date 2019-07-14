eb2d08c056718061515cf13386a72093bb79634f
ac101m <mofty101@gmail.com>
2019-07-14 14:26:37 +0100

    Add dedicated screen quad class for lighting pass

---
+++ include/screenQuad.hpp
@@ -0,0 +1,50 @@
+#ifndef DEFER_SCREENQUAD_INCLUDED
+#define DEFER_SCREENQUAD_INCLUDED
+
+
+// Standard
+#include <vector>
+
+// External
+#include <GLT/Texture.hpp>
+
+// This project
+#include <mesh.hpp>
+
+
+class ScreenQuad : public GLT::Mesh {
+public:
+
+  // Constructor, construct the base mesh
+  ScreenQuad(std::vector<GLT::Texture> textures) :
+    GLT::Mesh(GenFullscreenQuadMesh(textures)) {
+  }
+
+  // Draw the fullscreen quad
+  void Draw(GLT::Camera& camera, GLT::ShaderProgram& shader, glm::mat4& m) {
+
+    // Bind textures to texture units
+    std::string name = "texture0";
+    for(unsigned i = 0; i < this->textures.size(); i++) {
+      name[7] = 48 + i;
+      shader.SetTexture(i, name, this->textures[i]);
+    }
+
+    // Use the shader in question
+    shader.Use();
+
+    // Draw the things
+    this->vertexBuffer.Bind();
+    glDrawElements(
+      GL_TRIANGLES,
+      this->vertexBuffer.GetIndexBufferLength(),
+      GL_UNSIGNED_INT, 0);
+    this->vertexBuffer.Unbind();
+
+    // Set texture back to texture 0
+    glActiveTexture(GL_TEXTURE0);
+  }
+};
+
+
+#endif // DEFER_SCREENQUAD_INCLUDED
