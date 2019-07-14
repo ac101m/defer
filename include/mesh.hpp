d0fc2d5bb59590b9559847eda718fdc1579dbb13
ac101m <mofty101@gmail.com>
2019-07-14 20:07:02 +0100

    Update to make use of new GLT mipmapping

--- include/mesh.hpp
+++ include/mesh.hpp
@@ -118,15 +118,14 @@ GLT::Mesh GenCubeMesh(void) {
 
   // Load the side texture
   std::vector<GLT::Texture> textures = {
-    GLT::Texture("textures/brownrock/colour.png"),
-    GLT::Texture("textures/brownrock/normal.png"),
-    GLT::Texture("textures/brownrock/roughness.png")};
+    GLT::Texture("textures/brownrock/colour.png", 16),
+    GLT::Texture("textures/brownrock/normal.png", 16),
+    GLT::Texture("textures/brownrock/roughness.png", 16)};
 
   // Set sampling modes on the textures to linear
   for(unsigned i = 0; i < textures.size(); i++) {
     textures[i].Parameteri(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    textures[i].Parameteri(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    textures[i].GenerateMipmaps();
+    textures[i].Parameteri(GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
   }
 
   // Package up into mesh and return
